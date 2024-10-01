f4ad8f850f2f92ce6858b6be9b39e2433a57d8d6
gabrieldeori <deori.pessoal@gmail.com>
2024-10-01 15:16:51 -0300

    feat: example 1.4

---
+++ secao_1_4/examples.py
@@ -0,0 +1,21 @@
+print('Imprime o comprimento da lista')
+numeros = [1, 2, 3, 4, 5]
+
+# Usa a função len() para calcular o comprimento da lista
+COMPRIMENTO = len(numeros)
+print(COMPRIMENTO)
+
+print('Função Soma:')
+# Definindo uma função chamada "soma"
+
+
+def soma(a, b):
+    resultado = a + b
+    return resultado
+
+
+# Chamando a função e armazenando o resultado em uma variável
+RESULTADO = soma(5, 3)
+
+# Imprimindo o resultado
+print(RESULTADO)
