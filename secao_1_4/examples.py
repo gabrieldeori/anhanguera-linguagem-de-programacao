759555527728c184f214ac939acff324340fa90d
gabrieldeori <deori.pessoal@gmail.com>
2024-10-01 15:19:19 -0300

    feat: examples 1.4 faltando

--- secao_1_4/examples.py
+++ secao_1_4/examples.py
@@ -1,7 +1,7 @@
 print('Imprime o comprimento da lista')
 numeros = [1, 2, 3, 4, 5]
 
-# Usa a função len() para calcular o comprimento da lista
+print('Usa a função len() para calcular o comprimento da lista')
 COMPRIMENTO = len(numeros)
 print(COMPRIMENTO)
 
@@ -18,4 +18,30 @@ def soma(a, b):
 RESULTADO = soma(5, 3)
 
 # Imprimindo o resultado
+print(RESULTADO)
+
+print('Definindo uma função chamada "e_par"')
+
+
+def e_par(numero):
+    if numero % 2 == 0:
+        return True
+    else:
+        return False
+
+
+print('Testando a função')
+NUMERO = 123120
+
+if e_par(NUMERO):
+
+    print(f'{NUMERO} é um número par.')
+
+else:
+
+    print(f'{NUMERO} não é um número par.')
+
+print('Função lambda:')
+SOMA = lambda a, b: a + b
+RESULTADO = SOMA(3, 4)
 print(RESULTADO)
