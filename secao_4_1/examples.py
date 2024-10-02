f1441c07865644c38ac71ac0a48e1422f7d108fc
gabrieldeori <deori.pessoal@gmail.com>
2024-10-02 19:19:50 -0300

    feat: 4.1 examples

---
+++ secao_4_1/examples.py
@@ -0,0 +1,19 @@
+from IPython.display import HTML
+
+
+# Criando uma página HTML usando Python
+html_code = '''
+<!DOCTYPE html>
+<html>
+    <head>
+    <title>Exemplo de Front-end com Python</title>
+    </head>
+    <body>
+    <h1>Olá, mundo!</h1>
+    <p>Esta é uma página web criada usando Python no Google Colab.</p>
+    </body>
+</html>
+'''
+
+# Exibindo a página HTML
+HTML(html_code)
