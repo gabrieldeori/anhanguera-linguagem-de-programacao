a037464d53b17a18b4596bdb2676610c9c1172e8
gabrieldeori <deori.pessoal@gmail.com>
2024-10-02 18:44:25 -0300

    feat: 3.4 Exercício gorjetas médias

--- secao_3_4/tips.py
+++ secao_3_4/tips.py
@@ -2,6 +2,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 
+# Totais
 sns.set(style='whitegrid')
 
 df = sns.load_dataset('tips')
@@ -22,4 +23,32 @@ plt.xlabel('Período (Time)')
 plt.ylabel('Total de Gastos')
 plt.title('Total de Gastos por Período (Almoço ou Jantar)')
 
+plt.show()
+
+# Médias
+plt.figure(figsize=(8, 5))
+
+sns.barplot(
+    x='time',
+    y='total_bill',
+    data=df,
+    estimator=sum,
+    ci=None,
+    palette='Set2'
+)
+
+plt.xlabel('Período (Time)')
+plt.ylabel('Média de Gastos')
+plt.title('Média de Gastos por Período (Almoço ou Jantar)')
+plt.show()
+
+
+# Média Período
+plt.figure(figsize=(8, 5))
+
+sns.barplot(x='time', y='total_bill', data=df, palette='Set3')
+
+plt.xlabel('Período (Time)')
+plt.ylabel('Média da Gorjeta')
+plt.title('Média da Gorjeta por Período (Almoço ou Jantar)')
 plt.show()
